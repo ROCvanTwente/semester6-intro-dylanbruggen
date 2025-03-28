6ecd20d8db64cabf0b7da643184e99309d8d8807
dylan ter bruggen <0337452@student.rocvantwente.nl>
2025-03-28 15:04:30 +0100

    securaty1

---
+++ Security/OPDRACHT S6-ASPSEC-05/OPDRACHT S6-ASPSEC-05/OPDRACHT S6-ASPSEC-05/Controllers/KlasController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OPDRACHT_S6_ASPSEC_05.Controllers
+{
+    [Authorize]
+    public class KlasController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public KlasController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> IOSD1()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var claims = await _userManager.GetClaimsAsync(user);
+
+            if (claims.Any(c => c.Type == "Klas" && c.Value == "IOSD1"))
+            {
+                return View();
+            }
+
+            return Forbid(); // Geen toegang
+        }
+    }
+}

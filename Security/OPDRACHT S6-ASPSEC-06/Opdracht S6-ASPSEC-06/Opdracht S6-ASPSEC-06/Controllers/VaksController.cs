ddbc5827f9ed17e52ddd60af42ed59d6cabc56b5
dylan ter bruggen <0337452@student.rocvantwente.nl>
2025-04-01 08:45:51 +0200

    testing needed

---
+++ Security/OPDRACHT S6-ASPSEC-06/Opdracht S6-ASPSEC-06/Opdracht S6-ASPSEC-06/Controllers/VaksController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Opdracht_S6_ASPSEC_06.Data;
+using Opdracht_S6_ASPSEC_06.Models;
+
+namespace Opdracht_S6_ASPSEC_06.Controllers
+{
+    public class VaksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VaksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vaks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Vakken.ToListAsync());
+        }
+
+        // GET: Vaks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vak = await _context.Vakken
+                .FirstOrDefaultAsync(m => m.VakId == id);
+            if (vak == null)
+            {
+                return NotFound();
+            }
+
+            return View(vak);
+        }
+
+        // GET: Vaks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vaks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VakId,VakNaam")] Vak vak)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vak);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vak);
+        }
+
+        // GET: Vaks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vak = await _context.Vakken.FindAsync(id);
+            if (vak == null)
+            {
+                return NotFound();
+            }
+            return View(vak);
+        }
+
+        // POST: Vaks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VakId,VakNaam")] Vak vak)
+        {
+            if (id != vak.VakId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vak);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VakExists(vak.VakId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vak);
+        }
+
+        // GET: Vaks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vak = await _context.Vakken
+                .FirstOrDefaultAsync(m => m.VakId == id);
+            if (vak == null)
+            {
+                return NotFound();
+            }
+
+            return View(vak);
+        }
+
+        // POST: Vaks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vak = await _context.Vakken.FindAsync(id);
+            if (vak != null)
+            {
+                _context.Vakken.Remove(vak);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VakExists(int id)
+        {
+            return _context.Vakken.Any(e => e.VakId == id);
+        }
+    }
+}
---
+++ Security/OPDRACHT S6-ASPSEC-06/Opdracht S6-ASPSEC-06/Opdracht S6-ASPSEC-06/Migrations/20250331192516_InitialCreate.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Opdracht_S6_ASPSEC_06.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Studenten",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Voornaam = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Achternaam = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Studenten", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vakken",
+                columns: table => new
+                {
+                    VakId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VakNaam = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vakken", x => x.VakId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToetsResultaten",
+                columns: table => new
+                {
+                    ResultaatId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    VakId = table.Column<int>(type: "int", nullable: false),
+                    Cijfer = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToetsResultaten", x => x.ResultaatId);
+                    table.ForeignKey(
+                        name: "FK_ToetsResultaten_Studenten_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Studenten",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ToetsResultaten_Vakken_VakId",
+                        column: x => x.VakId,
+                        principalTable: "Vakken",
+                        principalColumn: "VakId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToetsResultaten_StudentId",
+                table: "ToetsResultaten",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToetsResultaten_VakId",
+                table: "ToetsResultaten",
+                column: "VakId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToetsResultaten");
+
+            migrationBuilder.DropTable(
+                name: "Studenten");
+
+            migrationBuilder.DropTable(
+                name: "Vakken");
+        }
+    }
+}
